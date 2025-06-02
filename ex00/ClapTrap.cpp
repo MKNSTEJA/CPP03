e6baf52e86ecd7baafcfcb7512c298a6662ff786
MKNSTEJA <chessmaniacs123@gmail.com>
2025-06-02 23:53:12 +0200

    Done with ex00

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmummadi <kmummadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 11:32:40 by kmummadi          #+#    #+#             */
+/*   Updated: 2025/06/01 19:19:59 by kmummadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "utils.hpp"
+
+// Constructors and Destructors
+ClapTrap::ClapTrap()
+    : _name("Anonymous"), _hitpoints(10), _energypoints(10), _attackdamage(10) {
+  std::string msg;
+  msg = "Default constructor for instance \"" + _name + "\" called.\n";
+  colorprint(msg, GREEN);
+}
+
+ClapTrap::ClapTrap(std::string name)
+    : _name(name), _hitpoints(10), _energypoints(10), _attackdamage(10) {
+  std::string msg;
+
+  msg = "Parametrised constructor for instance \"" + _name + "\" called.\n";
+  colorprint(msg, GREEN);
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+    : _name(copy.getName()), _hitpoints(copy.getHitPoints()),
+      _energypoints(copy.getEnergyPoints()),
+      _attackdamage(copy.getAttackDamage()) {
+  std::string msg;
+
+  msg = "Copy constructor for instance \"" + _name + "\" called.\n";
+  colorprint(msg, GREEN);
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &copy) {
+  std::string msg;
+
+  msg = "Copy assignment overload called\n";
+  colorprint(msg, GREEN);
+  if (this != &copy) {
+    _name = copy.getName();
+    _attackdamage = copy.getAttackDamage();
+    _energypoints = copy.getEnergyPoints();
+    _hitpoints = copy.getAttackDamage();
+  }
+  return (*this);
+}
+
+ClapTrap::~ClapTrap() {
+  std::string msg;
+
+  msg = "Destructor for instance \"" + _name + "\" called.\n";
+  colorprint(msg, GREEN);
+}
+
+// Getter functions
+std::string ClapTrap::getName() const { return (_name); }
+
+unsigned int ClapTrap::getAttackDamage() const { return (_attackdamage); }
+
+unsigned int ClapTrap::getEnergyPoints() const { return (_energypoints); }
+
+unsigned int ClapTrap::getHitPoints() const { return (_hitpoints); }
+
+// Setter functions
+void ClapTrap::setName(std::string name) { _name = name; }
+
+void ClapTrap::setHitPoints(unsigned int hitpoints) { _hitpoints = hitpoints; }
+
+void ClapTrap::setEnergyPoints(unsigned int energypoints) {
+  _energypoints = energypoints;
+}
+
+void ClapTrap::setAttackDamage(unsigned int attackdamage) {
+  _attackdamage = attackdamage;
+}
+
+// Subject functions
+void ClapTrap::attack(const std::string &target) {
+  if (_energypoints > 0 && _hitpoints > 0) {
+    --_energypoints;
+    std::cout << "Claptrap " + _name + " attacks " + target + ", causing"
+              << _attackdamage << "points of damage!\n";
+  } else if (_hitpoints == 0) {
+    colorprint("ClapTrap " + _name + " lacks hitpoints to attack\n", ORANGE);
+  } else if (_energypoints == 0) {
+    colorprint("ClapTrap " + _name + "lacks energypoints to attack\n", ORANGE);
+  }
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+  std::cout << "Claptrap " << _name << " has taken -" << amount
+            << "HP damage\n";
+  if (_hitpoints >= amount) {
+    _hitpoints -= amount;
+    std::cout << "HP left: " << _hitpoints << "\n";
+  } else {
+    colorprint(_name + " died!\n", ORANGE);
+  }
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+  if (_energypoints > 0) {
+    _energypoints--;
+    _hitpoints += amount;
+    std::cout << "Claptrap" << _name << " has gained" << amount << "HP\n";
+  } else
+    colorprint("Claptrap " + _name + "lacks energypoints to repair itself\n",
+               ORANGE);
+}
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmummadi <kmummadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 11:22:45 by kmummadi          #+#    #+#             */
+/*   Updated: 2025/05/30 13:35:07 by kmummadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ClapTrap {
+private:
+  std::string _name;
+  unsigned int _hitpoints;
+  unsigned int _energypoints;
+  unsigned int _attackdamage;
+
+public:
+  // Constructors and Destructor
+  ClapTrap();                                // Default constructor
+  ClapTrap(std::string name);                // Parametrized constructor
+  ClapTrap(const ClapTrap &copy);            // Copy constructor
+  ClapTrap &operator=(const ClapTrap &copy); // Assignment Operator overload
+  ~ClapTrap();                               // Destructor
+
+  // Subject fucntions
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+
+  // Getter functions
+  std::string getName() const;
+  unsigned int getHitPoints() const;
+  unsigned int getEnergyPoints() const;
+  unsigned int getAttackDamage() const;
+
+  // Setter functions
+  void setName(std::string name);
+  void setHitPoints(unsigned int hitpoints);
+  void setEnergyPoints(unsigned int energypoints);
+  void setAttackDamage(unsigned int attackdamage);
+};
